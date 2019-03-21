879db782cf44f6d33400e6a5e9691bf57bbea72d
Hugo Alvarez <hugoferneya@hotmail.com>
2019-03-20 17:00:42 -0700

    cambios al index.js

--- index.js
+++ index.js
@@ -1,6 +1,26 @@
 // JavaScript Document
+
+
+loadHtml = function(html){
+  $('#content').load(html);
+}
+
+
+loadCSS = function(href, id) {
+  var cssLink = $("<link rel='stylesheet' type='text/css' href='"+href+"' id='"+id+"'>");
+  $("head").append(cssLink); 
+ };
+
+/**
+ * function to load a given js file 
+ */ 
+ loadJS = function(src, id) {
+  var jsLink = $("<script type='text/javascript' src='"+src+"' id='"+id+"'>");
+  $("head").append(jsLink); 
+ }; 
+
+
 $(document).ready(function(){
-   
-   $('#content').load("app/artists/artists.html");
+
 
 });
