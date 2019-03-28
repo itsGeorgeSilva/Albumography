58d97a55fa9ff82d40d600744b774b5146c4ec11
Hugo Alvarez <hugoferneya@hotmail.com>
2019-03-28 05:47:54 -0700

    elementos de los videos agregados

---
+++ app/videos/videos.js
@@ -0,0 +1,12 @@
+// JavaScript Document
+
+$(document).ready(function(){
+  $("#content").css({
+    "color": "white",
+    "content": "",
+    "clear": "both",
+    "display": "table",
+    "background": "black",
+    "padding-bottom": "5rem"
+  });
+});
