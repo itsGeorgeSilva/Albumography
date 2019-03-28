d4d1b9347919a137e0723682fb6d7a9186b972ec
Hugo Alvarez <hugoferneya@hotmail.com>
2019-03-28 06:55:42 -0700

    retoques al home

--- app/home/home.js
+++ app/home/home.js
@@ -1,5 +1,17 @@
 // JavaScript Document
 
 $(document).ready(function(){
-  
+  $("#content").css({
+    "width": "100%";
+    "height": "100%;"
+    "color": "white",
+    "content": "",
+    "clear": "both",
+    "display": "table",
+    "background": "-webkit-linear-gradient(right, #fe87db, #ff9920)",
+    "background": "-o-linear-gradient(right, #fe87db, #ff9920)",
+    "background": "-moz-linear-gradient(right, #fe87db, #ff9920)",
+    "background": "linear-gradient(to left, #fe87db, #ff9920)",
+    "padding-bottom": "5rem"
+  });
 });
